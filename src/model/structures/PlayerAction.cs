420f28c28e8ede89d47868186bcc96acf8457dd4
Kurenkov <demel654@gmail.com>
2019-11-28 21:28:45 +0300

    first commit

---
+++ src/model/structures/PlayerAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcg
+{
+    struct PlayerAction
+    {
+        public int PlayerId { get; set; }
+        public ActionType Type { get; set; }
+
+        public PlayerAction(int playerId, ActionType type)
+        {
+            this.PlayerId = playerId;
+            this.Type = type;
+        }
+    }
+}
