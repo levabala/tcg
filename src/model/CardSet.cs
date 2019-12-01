d567c7c1d399b9d786d8e2e08cb0476ed7c0d7b7
levabala <levabala@ya.ru>
2019-12-01 22:17:41 +0300

    Flash Heal card completed. need tests

--- src/model/CardSet.cs
+++ src/model/CardSet.cs
@@ -6,15 +6,7 @@ namespace tcg
   static class CardSet
   {
     static Dictionary<string, AlmostAbstractCard> cards = new Dictionary<string, AlmostAbstractCard>() {
-          {"Flash Heal", new AlmostAbstractCard(1, 0, 0, (state, args) => {
-            // args: [playerIndexToHeal, cardIndexToHeal]
-            int playerIndexToHeal = (int)args[0];
-            int cardIndexToHeal = (int)args[1];
-
-            Card card = state.Players[playerIndexToHeal].CardInHand[cardIndexToHeal];
-
-            return state;
-          })}
+          {"Flash Heal", new AlmostAbstractCard(1, 0, 0, (state, args) => ActionSet.PackAction(state, ActionType.Heal, args)(state))}
         };
   }
 }
