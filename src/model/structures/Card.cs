7f00b91c61e9fda5ced0d630f0bd4201a16c73a4
levabala <levabala@ya.ru>
2019-12-13 12:03:57 +0300

    pads

--- src/model/structures/Card.cs
+++ src/model/structures/Card.cs
@@ -45,32 +45,33 @@ namespace tcg
 
     public override string ToString()
     {
+      Func<string, string, string, string, string, string> cardStringGenerator = (name, mana, hp, attack, description) =>
+        string.Format("{0} {1} {2} {3}\n\t{4}\n", name.PadRight(20, ' '), mana.PadLeft(10, ' '), hp.PadLeft(20, ' '), attack.PadLeft(35, ' '), description);
+
       string MPstring = string.Format("Mana cost: {0}\t", ManaCost).Pastel(Color.MediumTurquoise);
-      string HPstring =  string.Format("HP: {0}", HP).Pastel(Color.HotPink);
+      string HPstring = string.Format("HP: {0}", HP).Pastel(Color.HotPink);
       string ATKstring = string.Format("Attack: {0}", Attack).Pastel(Color.Gold);
       if (this.previous == null)
-        return string.Format("{0,-20} {1} {2}\t\t {3}\n\t{4}\n", Name, MPstring, HPstring, ATKstring, Description);
+        return cardStringGenerator(Name, MPstring, HPstring, ATKstring, Description);
       else if (this.previous.Equals(this))
-        return string.Format("{0,-20} {1} {2}+0\t\t {3}+0\n\t{4}\n", Name, MPstring, HPstring, ATKstring, Description);
-      else{
+        return cardStringGenerator(Name, MPstring, HPstring, ATKstring, Description);
+      else
+      {
 
         if (this.HP != this.previous.HP)
-          if(this.HP > this.previous.HP)
+          if (this.HP > this.previous.HP)
             HPstring += string.Format(" (+{0})", this.HP - this.previous.HP).Pastel(Color.Lime);
           else
             HPstring += string.Format(" (-{0})", this.previous.HP - this.HP).Pastel(Color.Crimson);
-        
-        HPstring += ",\t";
+
 
         if (this.Attack != this.previous.Attack)
-          if(this.Attack > this.previous.Attack)
+          if (this.Attack > this.previous.Attack)
             ATKstring += string.Format(" (+{0})", this.Attack - this.previous.Attack).Pastel(Color.Lime);
           else
             ATKstring += string.Format(" (-{0})", this.previous.Attack - this.Attack).Pastel(Color.Crimson);
-        
-        ATKstring += ",\t";
 
-        return string.Format("{0,-16} {1} {2}\t\t {3}\n\t{4}\n", Name, MPstring, HPstring, ATKstring, Description);
+        return cardStringGenerator(Name, MPstring, HPstring, ATKstring, Description);
       }
     }
 
